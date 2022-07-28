cfe496735c241a1cfae91c65d0adb9dffa0cc91f
Justin Mathew <web@jstn.ly>
2022-07-28 15:54:49 +0700

    Removed next-theme.d.ts after upgrading package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import '@/styles/globals.css'
 import React from 'react'
 import { AppProps } from 'next/app'
 import { CookiesProvider } from 'react-cookie'
+import { ThemeProvider } from 'next-themes'
 
 function App({ Component, pageProps }: AppProps) {
     return (
         <CookiesProvider>
-            <Component {...pageProps} />
+            <ThemeProvider>
+                <Component {...pageProps} />
+            </ThemeProvider>
         </CookiesProvider>
     )
 }
