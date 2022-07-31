6ef3486c8e4d0e00881a2c5ff917924ec3a6db54
Justin Mathew <web@jstn.ly>
2022-07-31 14:46:16 +0700

    Refactored DarkModeButton.tsx

--- src/components/ui/DarkModeButton.tsx
+++ src/components/ui/DarkModeButton.tsx
@@ -17,28 +17,26 @@ const DarkModeButton: FC<{ iconOnly?: boolean; className?: string }> = ({
 
     return (
         <Menu as="div" className="relative inline-block text-left">
-            <div>
-                <Menu.Button className={className}>
-                    {theme === 'light' && (
-                        <>
-                            <SunIcon className="inline-block h-4 w-4" />
-                            {iconOnly || <span className="ml-1.5">Light</span>}
-                        </>
-                    )}
-                    {theme === 'dark' && (
-                        <>
-                            <MoonIcon className="inline-block h-4 w-4" />
-                            {iconOnly || <span className="ml-1.5">Dark</span>}
-                        </>
-                    )}
-                    {theme === 'system' && (
-                        <>
-                            <DesktopComputerIcon className="inline-block h-4 w-4" />
-                            {iconOnly || <span className="ml-1.5">Dark</span>}
-                        </>
-                    )}
-                </Menu.Button>
-            </div>
+            <Menu.Button className={className}>
+                {theme === 'light' && (
+                    <>
+                        <SunIcon className="inline-block h-4 w-4" />
+                        {iconOnly || <span className="ml-1.5">Light</span>}
+                    </>
+                )}
+                {theme === 'dark' && (
+                    <>
+                        <MoonIcon className="inline-block h-4 w-4" />
+                        {iconOnly || <span className="ml-1.5">Dark</span>}
+                    </>
+                )}
+                {theme === 'system' && (
+                    <>
+                        <DesktopComputerIcon className="inline-block h-4 w-4" />
+                        {iconOnly || <span className="ml-1.5">Dark</span>}
+                    </>
+                )}
+            </Menu.Button>
             <Transition
                 as={Fragment}
                 enter="transition ease-out duration-100"
@@ -47,94 +45,45 @@ const DarkModeButton: FC<{ iconOnly?: boolean; className?: string }> = ({
                 leave="transition ease-in duration-75"
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95">
-                <Menu.Items className="pointer-events-auto absolute right-0 z-10 mt-2 w-32 origin-top-right divide-y divide-slate-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Items className="pointer-events-auto absolute right-0 z-10 mt-2 w-32 origin-top-right divide-y divide-slate-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-slate-800">
                     <div className="px-2 py-2">
-                        <Menu.Item>
-                            {({ active }) => (
-                                <button
-                                    type="button"
-                                    onClick={() => {
-                                        setTheme('light')
-                                    }}
-                                    className={`${
-                                        active
-                                            ? 'bg-slate-100 text-slate-900'
-                                            : 'text-slate-600'
-                                    } group flex w-full items-center rounded-md px-2 py-2 text-sm font-medium`}>
-                                    <div
-                                        className={
-                                            (theme === 'light' && 'text-blue') +
-                                            ' mr-2 flex h-6 w-6 flex-none items-center justify-center rounded-md bg-white shadow ring-1 ring-slate-900/10'
-                                        }>
-                                        <SunIcon
-                                            className={
-                                                (theme === 'light'
-                                                    ? 'text-blue'
-                                                    : 'text-slate-400 group-hover:text-slate-500') +
-                                                ' h-4 w-4'
-                                            }
-                                        />
-                                    </div>
-                                    Light
-                                </button>
-                            )}
-                        </Menu.Item>
-                        <Menu.Item>
-                            {({ active }) => (
-                                <button
-                                    type="button"
-                                    onClick={() => {
-                                        setTheme('dark')
-                                    }}
-                                    className={`${
-                                        active
-                                            ? 'bg-slate-100 text-slate-900'
-                                            : 'text-slate-600'
-                                    } group flex w-full items-center rounded-md px-2 py-2 text-sm font-medium`}>
-                                    <div
-                                        className={
-                                            (theme === 'dark' && 'text-blue') +
-                                            ' mr-2 flex h-6 w-6 flex-none items-center justify-center rounded-md bg-white shadow ring-1 ring-slate-900/10'
-                                        }>
-                                        <MoonIcon
-                                            className={
-                                                (theme === 'dark'
-                                                    ? 'text-blue'
-                                                    : 'text-slate-400 group-hover:text-slate-500') +
-                                                ' h-4 w-4'
-                                            }
-                                        />
-                                    </div>
-                                    Dark
-                                </button>
-                            )}
-                        </Menu.Item>
-                        <Menu.Item>
-                            {({ active }) => (
-                                <button
-                                    type="button"
-                                    onClick={() => {
-                                        setTheme('system')
-                                    }}
-                                    className={`${
-                                        active
-                                            ? 'bg-slate-100 text-slate-900'
-                                            : 'text-slate-600'
-                                    } group flex w-full items-center rounded-md px-2 py-2 text-sm font-medium`}>
-                                    <div className="mr-2 flex h-6 w-6 flex-none items-center justify-center rounded-md bg-white shadow ring-1 ring-slate-900/10">
-                                        <DesktopComputerIcon
-                                            className={
-                                                (theme === 'system'
-                                                    ? 'text-blue'
-                                                    : 'text-slate-400 group-hover:text-slate-500') +
-                                                ' h-4 w-4'
-                                            }
-                                        />
-                                    </div>
-                                    System
-                                </button>
-                            )}
-                        </Menu.Item>
+                        {Themes().map(themeItem => {
+                            const ThemeIcon = themeItem.icon
+                            return (
+                                <Menu.Item key={themeItem.id}>
+                                    {({ active }) => (
+                                        <button
+                                            type="button"
+                                            onClick={() => {
+                                                setTheme(themeItem.name)
+                                            }}
+                                            className={`${
+                                                active
+                                                    ? 'bg-slate-100 text-slate-900 dark:bg-slate-700 dark:text-slate-100'
+                                                    : 'text-slate-600 dark:text-slate-400'
+                                            } group flex w-full items-center rounded-md px-2 py-2 text-sm font-medium`}>
+                                            <div
+                                                className={
+                                                    (themeItem.name === theme &&
+                                                        'text-blue') +
+                                                    ' mr-2 flex h-6 w-6 flex-none items-center justify-center rounded-md bg-white shadow ring-1 ring-slate-900/10 dark:border dark:border-slate-700 dark:bg-slate-800 dark:shadow-none'
+                                                }>
+                                                <ThemeIcon
+                                                    className={
+                                                        (themeItem.name ===
+                                                        theme
+                                                            ? 'text-blue'
+                                                            : 'text-slate-400 group-hover:text-slate-500') +
+                                                        ' h-4 w-4'
+                                                    }
+                                                />
+                                            </div>
+                                            {themeItem.label}
+                                        </button>
+                                    )}
+                                </Menu.Item>
+                            )
+                        })}
                     </div>
                 </Menu.Items>
             </Transition>
@@ -142,3 +91,35 @@ const DarkModeButton: FC<{ iconOnly?: boolean; className?: string }> = ({
     )
 }
 export default DarkModeButton
+
+/**
+ * Define your themes here.
+ */
+export function Themes() {
+    return [
+        {
+            id: 1,
+            name: 'light',
+            label: 'Light',
+            icon: ({ className }: { className: string }) => (
+                <SunIcon className={className} />
+            )
+        },
+        {
+            id: 2,
+            name: 'dark',
+            label: 'Dark',
+            icon: ({ className }: { className: string }) => (
+                <MoonIcon className={className} />
+            )
+        },
+        {
+            id: 3,
+            name: 'system',
+            label: 'System',
+            icon: ({ className }: { className: string }) => (
+                <DesktopComputerIcon className={className} />
+            )
+        }
+    ]
+}
